f07b84bb8f437d5c9f2c94ea7d2a9b3afeb60548
admapop <adrianmpop@gmail.com>
2021-11-01 16:50:59 +0100

    feat: add navigation to app

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { Text } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { theme } from './src/infrastructure/theme';
 import {
   useFonts as useOswald,
@@ -9,21 +13,78 @@ import {
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 import { Restaurants } from './src/screens/Restaurants';
 
+const Tab = createBottomTabNavigator();
+
+enum IconNames {
+  Restaurant = 'Restaurants',
+  Map = 'Map',
+  Settings = 'Settings',
+}
+
 export default function App() {
-  let [oswaldLoaded] = useOswald({
+  const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
-  let [latoLoaded] = useLato({
+  const [latoLoaded] = useLato({
     Lato_400Regular,
   });
 
-  if (!oswaldLoaded || !latoLoaded) return null;
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  function Map() {
+    return <Text>Map</Text>;
+  }
+  function Settings() {
+    return <Text>Settings</Text>;
+  }
 
   return (
-    <>
+    <NavigationContainer>
       <ThemeProvider theme={theme}>
-        <Restaurants />
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              const { name } = route;
+              switch (name) {
+                case IconNames.Restaurant:
+                  return (
+                    <Ionicons
+                      name={focused ? 'restaurant' : 'restaurant-outline'}
+                      size={size}
+                      color={color}
+                    />
+                  );
+
+                case IconNames.Map:
+                  return (
+                    <Ionicons
+                      name={focused ? 'map' : 'map-outline'}
+                      size={size}
+                      color={color}
+                    />
+                  );
+
+                default:
+                  return (
+                    <Ionicons
+                      name={focused ? 'settings' : 'settings-outline'}
+                      size={size}
+                      color={color}
+                    />
+                  );
+              }
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen name="Restaurants" component={Restaurants} />
+          <Tab.Screen name="Map" component={Map} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
       </ThemeProvider>
-    </>
+    </NavigationContainer>
   );
 }
